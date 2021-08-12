f53425e72ebfd26998867c41c870873fb082a2ca
krilatiy <pos@list.ru>
2021-08-12 14:30:31 +0300

    2.6 comments

--- Chapter02-Map_insertion_hint/Map_insertion_hint.cpp
+++ Chapter02-Map_insertion_hint/Map_insertion_hint.cpp
@@ -16,17 +16,22 @@ int main()
     map_type m{ {"b", 1}, {"c", 2}, {"d", 3} };
 
     auto insert_it(end(m));
-    for (const auto& s : { "z", "y", "x", "w", "v" }) {
+    for (const auto& s : { "z", "y", "x", "w", "v" }) 
+	{
         insert_it = m.insert(insert_it, { s, 1 });
     }
 
-    // In this example line, the hint is wrong.
-    // It points past the last element, but the "0" string will be inserted
-    // at the front. Therefore it does not have a better runtime performance
-    // than insert without a hint parameter!
+    // Чтобы продемонстрировать, как не нужно решать задачу, вставим строку, 
+    // которая окажется с левого края ассоциативного массива, но зададим для 
+    // нее неправильную подсказку, указывающую на крайнюю правую позицию
+    // ассоциативного массива — end:
     m.insert(end(m), { "a", 1 });
 
-    for (const auto& [key, value] : m) {
+	// При выводе можно убедиться что ошибочная подсказка ничего не испортила
+	// По сути в случае неправильной подсказки вставка будет происходить 
+	// за большее время чем при правильной подсказке
+    for (const auto& [key, value] : m) 
+	{
         std::cout << "\"" << key << "\": " << value << ", ";
     }
     std::cout << '\n';
